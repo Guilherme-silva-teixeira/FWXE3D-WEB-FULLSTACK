f5b8051d0412e8b591cc1579cd8d23f1190e18a3
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-04-21 10:38:49 -0300

    Create script.js

---
+++ test-styles/script.js
@@ -0,0 +1,20 @@
+let circle = document.getElementById("circle");
+let circleHeight = circle.style.height;
+let circleWidth = circle.style.width;
+var colors = ['#f00','#0f0','#00f'];
+
+function click()
+{
+    let circle = document.getElementById("circle");
+    let circleHeight = circle.style.height;
+    let circleWidth = circle.style.width;
+    var colors = ['#f00','#0f0','#00f'];
+    let number = 13;
+    return 13;
+    
+circleHeight = circleHeight + click();
+circleWidth = circleWidth + click();
+}
+
+circleHeight = circleHeight + click();
+circleWidth = circleWidth + click();
