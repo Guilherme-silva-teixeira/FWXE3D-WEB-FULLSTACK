9d413a3da41dfeeeb87fc312a1bb12cf5856623c
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-04-22 14:50:15 -0300

    Update script.js

--- test-styles/script.js
+++ test-styles/script.js
@@ -1,4 +1,5 @@
-let circle = document.getElementById("circle");
+//teste do código 00
+/*let circle = document.getElementById("circle");
 let circleHeight = circle.style.height;
 let circleWidth = circle.style.width;
 var colors = ['#f00','#0f0','#00f'];
@@ -18,3 +19,30 @@ circleWidth = circleWidth + click();
 
 circleHeight = circleHeight + click();
 circleWidth = circleWidth + click();
+*/
+
+//teste do código 01
+/*
+let circle = document.getElementById("circle");
+let circleHeight = circle.style.height;
+let circleWidth = circle.style.width;
+var colors = ['#f00','#0f0','#00f'];
+let colorIndex = 0;
+
+function click() {
+    circleHeight = parseInt(circleHeight) + 13 + "px";
+    circleWidth = parseInt(circleWidth) + 13 + "px";
+    circle.style.height = circleHeight;
+    circle.style.width = circleWidth;
+    circle.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+    let ch = document.getElementById("circle").style.height;
+    if(ch => "400px")
+    {
+        
+    }
+}
+
+circle.addEventListener("click", click);
+*/
+//tentativa:
