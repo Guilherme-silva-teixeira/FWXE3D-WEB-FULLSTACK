5aaeba30af5adfa5cedbffb6714210e70d9886c4
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-04-22 15:24:15 -0300

    Create script.js

---
+++ test cauculator - IOS/script.js
@@ -0,0 +1,15 @@
+let display = document.getElementById("display");
+
+function cauculate()
+{
+    let display = document.getElementById("display");
+    let one = document.getElementById("A1");
+    let two = document.getElementById("A2");
+    let three = document.getElementById("A3");
+    let four = document.getElementById("A4");
+    let five = document.getElementById("A5");
+    let six = document.getElementById("A6");
+    let seven = document.getElementById("A7");
+    let eigth = document.getElementById("A8");
+    let nine = document.getElementById("A9");
+}
